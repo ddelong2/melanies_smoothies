d5c4fad56aa3ab4eddf1211dfaf0e2fccda5bac1
ddelong2 <delonggt@gmail.com>
2025-04-14 16:51:19 +0200

    Update streamlit_app.py
    nutrition score

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,7 @@ if ingredients_list:
         success_message = ''
         success_message = "Your Smoothie is ordered, "+name_on_order +" !"
         st.success(success_message, icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
